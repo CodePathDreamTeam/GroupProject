6bc5973eba376fd97360673fcec6c979c7e1b75e
Brandon Aubrey <brandonaubrey@brandons-mbp-2>
2017-04-25 00:00:39 -0700

    started working on currency conversion

--- GroupProject/FixerClient.swift
+++ GroupProject/FixerClient.swift
@@ -10,12 +10,13 @@ import Foundation
 
 class FixerClient {
     
-    let urlString = "http://api.fixer.io/latest?base=USD"
+    let baseUrl = "https://api.fixer.io/latest?base="
     
     static let sharedInstance = FixerClient()
     
-    func getJSON(_ url: String, completionHandler: @escaping ((_ json: AnyObject) -> Void)) {
-        let nsURL = URL(string: url)!
+    func getJSON(home: String, destination: String, completionHandler: @escaping ((_ json: AnyObject) -> Void)) {
+        let urlString = baseUrl + home
+        let nsURL = URL(string: urlString)!
         let session = URLSession.shared
         let task = session.dataTask(with: nsURL, completionHandler: { data, response, error -> Void in
             if error != nil{
@@ -23,13 +24,17 @@ class FixerClient {
             }
             if data != nil {
                 let jsonData = (try! JSONSerialization.jsonObject( with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)) as! [String:Any]
-                
-                completionHandler(jsonData as AnyObject)
+                //print(jsonData)
+                if let rates = jsonData["rates"] as? [String:AnyObject] {
+                    completionHandler(rates[destination]!)
+                } else {
+                //print(jsonData["Results"] as? NSDictionary[destination])
+                completionHandler(1 as AnyObject)
+                }
             }
             session.invalidateAndCancel()
         })
         task.resume()
-        
     }
-    
 }
+
--- GroupProject/HomeViewController.swift
+++ GroupProject/HomeViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class HomeViewController: DashBaseViewController {
 
+    @IBOutlet weak var homeCurrencyTF: UITextField!
+    @IBOutlet weak var destinationCurrencyTF: UITextField!
+
+    var rate: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        FixerClient.sharedInstance.getJSON(home: "USD", destination: "JPY", completionHandler: {
+             rate in DispatchQueue.main.async {
+                self.rate = rate as! Double
+                self.homeCurrencyTF.text = "1"
+                self.destinationCurrencyTF.text = "\(self.rate!)"
+            }
+        })
         // Do any additional setup after loading the view.
     }
 
@@ -20,5 +32,8 @@ class HomeViewController: DashBaseViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension HomeViewController: UITextFieldDelegate {
+    
 }
