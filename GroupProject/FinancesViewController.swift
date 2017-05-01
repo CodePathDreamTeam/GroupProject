065876b1c348804355f6ed104e700e750870c28b
Nana <narayanan_eie@yahoo.com>
2017-04-30 18:39:52 -0700

    OCR Framework Integration for Text recognization

--- GroupProject/FinancesViewController.swift
+++ GroupProject/FinancesViewController.swift
@@ -10,15 +10,112 @@ import UIKit
 
 class FinancesViewController: DashBaseViewController {
 
+    @IBOutlet weak var importTextLabel: UILabel!
+
+    var activityIndicator:UIActivityIndicatorView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func importImage(_ sender: AnyObject) {
+        view.endEditing(true)
+
+        let imagePickerActionSheet = UIAlertController(title: "Snap/Upload Photo",
+                                                       message: nil, preferredStyle: .actionSheet)
+
+        // Check for device compabtability before adding Camera button
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePickerActionSheet.addAction(UIAlertAction(title: "Take Photo",
+                                                           style: .default) { (alert) -> Void in
+                                                            let imagePicker = UIImagePickerController()
+                                                            imagePicker.delegate = self
+                                                            imagePicker.sourceType = .camera
+                                                            self.present(imagePicker,animated: true,completion: nil)
+            })
+        }
+
+        // Option to pick existing images
+        imagePickerActionSheet.addAction(UIAlertAction(title: "Choose Existing",
+                                                       style: .default) { (alert) -> Void in
+                                                        let imagePicker = UIImagePickerController()
+                                                        imagePicker.delegate = self
+                                                        imagePicker.sourceType = .photoLibrary
+                                                        self.present(imagePicker,animated: true,completion: nil)
+        })
+
+        // Option to cancel the import action
+        imagePickerActionSheet.addAction(UIAlertAction(title: "Cancel",
+                                                       style: .cancel) { (alert) -> Void in
+        })
+
+        present(imagePickerActionSheet, animated: true, completion: nil)
     }
 
+    func performImageRecognition(_ image: UIImage) {
+
+        let tesseract = G8Tesseract()
+
+        tesseract.language = "eng+fra"
+        tesseract.engineMode = .tesseractCubeCombined
+        tesseract.pageSegmentationMode = .auto
+        tesseract.maximumRecognitionTime = 60.0
+        tesseract.image = image.g8_blackAndWhite()
+
+        tesseract.recognize()
+
+        importTextLabel.text = tesseract.recognizedText
+        removeActivityIndicator()
+    }
+
+    func scale(image: UIImage, maxDimension: CGFloat) -> UIImage {
+
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        var scaleFactor: CGFloat
+
+        if image.size.width > image.size.height {
+            scaleFactor = image.size.height / image.size.width
+            scaledSize.width = maxDimension
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            scaleFactor = image.size.width / image.size.height
+            scaledSize.height = maxDimension
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+
+        UIGraphicsBeginImageContext(scaledSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: scaledSize.width, height: scaledSize.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
+
+    func addActivityIndicator() {
+        activityIndicator = UIActivityIndicatorView(frame: view.bounds)
+        activityIndicator.activityIndicatorViewStyle = .whiteLarge
+        activityIndicator.backgroundColor = UIColor(white: 0, alpha: 0.25)
+        activityIndicator.startAnimating()
+        view.addSubview(activityIndicator)
+    }
+
+    func removeActivityIndicator() {
+        activityIndicator.removeFromSuperview()
+        activityIndicator = nil
+    }
+
+}
+
+extension FinancesViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let selectedPhoto = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let scaledImage = scale(image: selectedPhoto, maxDimension: 640)
+
+        addActivityIndicator()
+
+        dismiss(animated:true, completion: {
+            self.performImageRecognition(scaledImage)
+        })
+    }
 }
