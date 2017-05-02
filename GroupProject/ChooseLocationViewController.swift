19e9babb5f07cefcbd4f687f4c7a3b6f4db018f2
ltran6 <Leland_Tran@Intuit.com>
2017-05-02 10:45:56 -0700

    Save location coordinates to user defaults

--- GroupProject/ChooseLocationViewController.swift
+++ GroupProject/ChooseLocationViewController.swift
@@ -53,7 +53,16 @@ extension ChooseLocationViewController: GMSAutocompleteViewControllerDelegate {
         print("Place address:  \(place.formattedAddress)")
         print("Place attributions: \(place.attributions)")
         print("Place coordinates: \(place.coordinate)")
+        print("Place latitude: \(place.coordinate.latitude)")
+        
+        let defaults = UserDefaults.standard
+        defaults.set("\(place.coordinate.latitude)", forKey: "latitude")
+        defaults.set("\(place.coordinate.longitude)", forKey: "longitude")
+        defaults.synchronize()
         dismiss(animated: true, completion: nil)
+        print("defaults[latitude]: \(defaults.object(forKey: "latitude") as? String)")
+        print("defaults[longitude]: \(defaults.object(forKey: "longitude") as? String)")
+        
     }
     
     func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
