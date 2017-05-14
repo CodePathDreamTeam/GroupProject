f4755b774c5a0a37ded54c6a9a587103b1e478a0
creyme <creyme.art@gmail.com>
2017-05-13 19:45:33 -0700

    set navbar to transparent

---
+++ GroupProject/NavBarTransparent.swift
@@ -0,0 +1,30 @@
+//
+//  NavBarTransparent.swift
+//  GroupProject
+//
+//  Created by CRISTINA MACARAIG on 5/13/17.
+//  Copyright © 2017 Brandon Aubrey. All rights reserved.
+//
+
+import UIKit
+
+class NavBarTransparent: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Customize Appearance
+        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
+        self.navigationBar.tintColor = .white
+        
+        self.navigationBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        self.navigationBar.shadowImage = UIImage()
+        self.navigationBar.barStyle = .black
+        self.navigationBar.isTranslucent = true
+        
+
+
+    
+    }
+}
