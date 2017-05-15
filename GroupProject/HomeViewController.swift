d35f4ab87518ae288245c801975488b69a43a087
creyme <creyme.art@gmail.com>
2017-05-14 21:14:11 -0700

    Stage files (minor ui edit)

--- GroupProject/HomeViewController.swift
+++ GroupProject/HomeViewController.swift
@@ -70,6 +70,15 @@ class HomeViewController: DashBaseViewController, UIScrollViewDelegate, UITextFi
 // @DAFAULT -----------------------------------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // UI
+        let trippinLogo = UIImage(named: "Trippin.png")
+        let logoImage = UIImageView(image: trippinLogo)
+        logoImage.frame.size.width = 78
+        logoImage.frame.size.height = 23
+        logoImage.contentMode = .scaleAspectFit
+        self.navigationItem.titleView = logoImage
 
         /*FixerClient.shared.getCurrencyRate(for: "JPY", relativeTo: "USD") {[weak self] (result) in
 
